37a45b447aad6976dfa7042fcce4801399aa1c5d
Nick Lee <44768335+SmallRunt@users.noreply.github.com>
2020-01-14 02:50:15 +0800

    Dialogue System done
    Dialogue system is working now.

---
+++ Ignote/Assets/_Scripts/Dialogue/Dialog.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    [TextArea (7,10)]
+    public string[]sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject dialogHolder;
+    public float timer;
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if(timer <= 5)
+        {
+            timer -= Time.deltaTime * 1;
+        }
+        if(timer <= 0)
+        {
+            StartCoroutine(NextSentence());
+        }
+
+        if(index + 1 == sentences.Length )
+        {
+            Destroy(dialogHolder);
+        }
+        Debug.Log(index);
+    }
+    
+
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+            {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    
+    IEnumerator NextSentence()
+    {
+        timer = 5;
+        yield return new WaitForSeconds(5);
+        NextDialog();
+    }
+
+    public void NextDialog()
+    {
+        if(index < sentences.Length -1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+}
