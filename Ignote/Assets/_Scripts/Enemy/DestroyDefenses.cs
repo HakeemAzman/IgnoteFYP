d650066bb6eb3d0b28d4d0c41b29175c17665728
Hakeem Azman <nurhakeemazman@gmail.com>
2020-01-16 02:34:17 +0800

    Fixed the miniboss trap.

---
+++ Ignote/Assets/_Scripts/Enemy/DestroyDefenses.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDefenses : MonoBehaviour
+{
+    public GameObject[] defenses;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            defenses[0].SetActive(false);
+            defenses[1].SetActive(false);
+            defenses[2].SetActive(false);
+        }
+    }
+}
