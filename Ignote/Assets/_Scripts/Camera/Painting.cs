531f9065acfafc8a317a938dfa69fb858213995d
HakeemAzman <nurhakeemazman@gmail.com>
2020-02-07 15:23:04 +0800

    Archduchess Chamber 60% done

---
+++ Ignote/Assets/_Scripts/Camera/Painting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+    [SerializeField] GameObject paintCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            paintCam.GetComponent<Animator>().Play("PaintingEnter");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            paintCam.GetComponent<Animator>().Play("PaintingExit");
+        }
+    }
+}
